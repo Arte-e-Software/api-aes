248db47fc949fb661c96b7820ce406a86d441d31
Arte-e-Software <aes.tec.br@gmail.com>
2022-11-18 14:44:13 -0300

    Crypto implementação

---
+++ back-end/routes/login-controller-route.js
@@ -0,0 +1,71 @@
+const express = require('express')
+    , router = express.Router()
+    , conn = require('../db/conn')
+    , sql = require('mssql')
+    , crypto = require('crypto')
+
+module.exports = router.post('/login/controller', (req, res) => {
+
+    let hashPW = crypto.createHash('sha256', process.env.HASH_SECRET)
+        .update(req.body.pw)
+        .digest('hex')
+
+    let db = conn['db_08310130000132']
+        , query = `
+        
+            SELECT
+                sysadminid
+                ,login
+                ,pw   
+                ,nome 
+                ,email
+                ,celular
+                ,crdate
+                ,isactive
+            FROM
+                sysadmin
+            WHERE
+                login ='${req.body.login}'
+            AND
+                pw = '${hashPW}'
+            AND 
+                isactive = 1
+                
+        `
+
+    sql.connect(db).then(() => { return sql.query(query) })
+        .then(result => {
+            sql.close();
+
+            let recordset = result.recordset[0]
+                , sysadminid = recordset.sysadminid
+                , login = recordset.login
+                , pw = recordset.pw
+                , nome = recordset.nome
+                , email = recordset.email
+                , celular = recordset.celular
+                , crdate = recordset.crdate
+                , isactive = recordset.isactive
+
+            // login ok
+            // criar o cookie
+            // criar o registro de login no banco
+            // manter a sessão via banco de dados com data, ip, etc
+
+            console.log('login ok')
+
+        })
+        .catch(err => {
+            sql.close();
+
+            console.log('login fail')
+            res.send({ message: 'Dados Incorretos' })
+
+        })
+    sql.on('error', err => {
+
+        console.log(err)
+
+    })
+
+})
